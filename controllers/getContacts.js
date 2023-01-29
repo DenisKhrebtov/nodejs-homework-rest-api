39b5127a8667432a38eaf7369c2e23b84df60704
DenisKhrebtov <khrebtovdenis@gmail.com>
2023-01-29 16:29:18 +0200

    fix mistakes

--- controllers/getContacts.js
+++ controllers/getContacts.js
@@ -5,7 +5,7 @@ const getContacts = async (req, res, next) => {
   const contacts = await Contact.find({});
 
   if (!contacts.length) {
-    return next(new HttpError(400, 'Contacts not found'));
+    return next(new HttpError(404, 'Contacts not found'));
   }
 
   res.json(contacts);
