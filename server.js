1e02161f232d1ccd8d3bf2d3b191b7a6494c15cb
DenisKhrebtov <khrebtovdenis@gmail.com>
2023-01-29 22:28:18 +0200

    fix port

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const app = require('./app');
 dotenv.config();
 mongoose.set('strictQuery', false);
 
-const { HOST_URI, PORT } = process.env;
+const { HOST_URI} = process.env;
 
 (async () => {
   try {
     await mongoose.connect(HOST_URI);
     console.log('Database connection successful');
 
-    app.listen(PORT, () => {
-      console.log(`Server running. Use our API on port: ${PORT}`);
+    app.listen(3000, () => {
+      console.log(`Server running. Use our API on port: 3000`);
     });
   } catch (error) {
     console.error('Error while connecting to mongodb', error.message);
