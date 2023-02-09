e28eec795d7ce6c0a51a9ccc83ba74f2720d0967
DenisKhrebtov <khrebtovdenis@gmail.com>
2023-02-09 16:27:02 +0200

    add hw6

--- src/controllers/users/uploadAvatar.js
+++ src/controllers/users/uploadAvatar.js
@@ -1,4 +1,3 @@
-// const path = require('path');
 const fs = require('fs/promises');
 const Jimp = require('jimp');
 const { User } = require('../../models');
--- src/schemas/joi/users/index.js
+++ src/schemas/joi/users/index.js
@@ -1,7 +1,11 @@
+
+
 const authUserSchema = require('./authUserSchema');
 const updateSubSchema = require('./updateSubSchema');
+const sendVerificationSchema = require('./sendVerificationSchema');
 
 module.exports = {
   authUserSchema,
   updateSubSchema,
+  sendVerificationSchema,
 };
